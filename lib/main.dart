80cc93eaa313bb29bcebaa3f220de7592c98d43e
d4ken <d-shikencho@techqiao.co.jp>
2024-02-12 20:12:01 +0900

    デバッグバナーを非表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:gemini_photex/pages/chat_screen.dart';
-
 import 'env/env.dart';
 
 void main() {
@@ -15,6 +14,7 @@ class ScaffoldExampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.light(useMaterial3: true),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
